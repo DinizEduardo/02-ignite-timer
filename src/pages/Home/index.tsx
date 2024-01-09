85f271806d9ec45a2a309fc2cd89a5e84a0a9730
Eduardo Diniz <eduardo.diniz987@gmail.com>
2024-01-08 22:19:39 -0300

    feat: typescript no form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -17,12 +17,20 @@ const newCycleFormValidationSchema = zod.object({
   minutesAmount: zod.number().min(5).max(60),
 })
 
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm({
+  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 25,
+    },
   })
 
-  function handleCreateNewCycle() {}
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    console.log(data)
+  }
 
   const task = watch('task')
   const isSubmitDisabled = !task
