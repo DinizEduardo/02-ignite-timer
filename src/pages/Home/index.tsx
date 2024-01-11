4bdebd05336696e954ff86320ce30c2aaecefdec
Eduardo Diniz <eduardo.diniz987@gmail.com>
2024-01-11 12:03:40 -0300

    feat: correção de bug e title da pagian

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -44,15 +44,16 @@ export function Home() {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   useEffect(() => {
+    let interval: number
     if (activeCycle) {
-      const interval = setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate),
         )
       }, 1000)
-
-      return () => clearInterval(interval)
     }
+
+    return () => clearInterval(interval)
   }, [activeCycle])
 
   function handleCreateNewCycle(data: NewCycleFormData) {
@@ -65,6 +66,7 @@ export function Home() {
 
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(newCycle.id)
+    setAmountSecondsPassed(0)
 
     reset()
   }
@@ -77,6 +79,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds} | Pomodoro}`
+    }
+  }, [minutes, seconds, activeCycle])
 
   const task = watch('task')
   const isSubmitDisabled = !task
